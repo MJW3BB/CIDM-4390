707f2d47c46be0e015c6746593cbbc8c0f5a75c6
Matthew Webb <mjwebb2@buffs.wtamu.edu>
2020-01-26 22:49:22 -0600

    Added assignment 2

---
+++ Assignment-2/Models/AddressModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesUI.Models
+{
+    public class AddressModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string BusinessName { get; set; }
+        public string BusinessRole { get; set; }
+        public string EmailAddress { get; set; }
+        public string OfficeNumber { get; set; }
+        public string CellNumber { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string Desc { get; set; }
+    }
+}
---
+++ Assignment-2/Models/ProgressModel.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesUI.Models
+{
+    public class ProgressModel
+    {
+        public string Project1 { get; set; }
+        public string Project2 { get; set; }
+        public string Project3 { get; set; }
+        public string Project4 { get; set; }
+        public string Project5 { get; set; }
+        public string Project6 { get; set; }
+        public string Project7 { get; set; }
+        public string Project8 { get; set; }
+        public string Project9 { get; set; }
+        public string Project10 { get; set; }
+    }
+}
